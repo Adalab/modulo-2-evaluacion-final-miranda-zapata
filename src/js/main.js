1f5292f37d7d58a5511ddd3cb3a67ab8c07cb94b
miranda-zapata <miranda.zapata@gmail.com>
2022-04-07 18:43:05 +0200

    start js. add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+// Obtención de datos HTML
+const cocktailList = document.querySelector('.cocktailList');
+const inputSearch = document.querySelector('.js-search');
+
+// Info del array
+let drinks = [];
+
+// Fetch a los cócteles del servidor
+fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch.value}`)
+  .then((response) => response.json())
+  .then((data) => {
+    // Guardar la información de los cócteles
+    drinks = data.drinks;
+  });
