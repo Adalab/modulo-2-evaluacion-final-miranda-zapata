5f8f997479dacb51f84103ad5540b205888fedfa
miranda-zapata <miranda.zapata@gmail.com>
2022-04-07 20:08:24 +0200

    paint/render function

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,42 @@
 'use strict';
-// Obtención de datos HTML
-const cocktailList = document.querySelector('.cocktailList');
+// HTML data
+const cocktailList = document.querySelector('.js-cocktailList');
 const inputSearch = document.querySelector('.js-search');
 
-// Info del array
+// Array info
 let drinks = [];
 
-// Fetch a los cócteles del servidor
-fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch.value}`)
+// Paint/render HTML
+function renderCocktailList() {
+  let html = '';
+  // Constante de la imagen de relleno
+  const emptyImg =
+    'https://via.placeholder.com/210x295/ffffff/666666/?text=drink';
+  for (const drinkItem of drinks) {
+    if (drinkItem.strDrinkThumb !== null) {
+      html += `<li>`;
+      html += `<img alt="Cóctel" class="cocktailImg" src="${drinkItem.strDrinkThumb}" />`;
+      html += `<h3>${drinkItem.strDrink}</h3>`;
+      html += `</li>`;
+    } else {
+      html += `<li>`;
+      html += `<img alt="Cóctel" class="cocktailImg" src="${emptyImg}" />`;
+      html += `<h3>${drinkItem.strDrink}</h3>`;
+      html += `</li>`;
+    }
+    cocktailList.innerHTML = html;
+  }
+}
+
+// Fetch cocktails from server
+fetch(
+  `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch.value}`
+)
   .then((response) => response.json())
   .then((data) => {
-    // Guardar la información de los cócteles
+    // Save cocktails' info
     drinks = data.drinks;
+
+    // Paint/render HTML
+    renderCocktailList();
   });
