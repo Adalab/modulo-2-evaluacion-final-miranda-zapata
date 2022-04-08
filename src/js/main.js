cc9f6e05f038d3ddbd64c6881c233c3edcea934c
miranda-zapata <miranda.zapata@gmail.com>
2022-04-08 13:08:48 +0200

    complete handle function to find favorites

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,14 @@
 'use strict';
-// HTML data
+// Datos HTML
 const cocktailList = document.querySelector('.js-cocktailList');
 const inputSearch = document.querySelector('.js-search');
 
-// Array info
+// Array/lista de todas las bebidas
 let drinks = [];
+// Array/lista de cócteles favoritos
+let favoriteCocktails = [];
 
-// Handle function
-function handleClickCocktail(event) {
-  console.log('Holis');
-  console.log(event.currentTarget.id);
-}
-
-// Listener (click event) for each cocktail
+// Listener (evento clic) para cada cóctel
 function cocktailListener() {
   const cocktailListItem = document.querySelectorAll('.js-cocktail');
   for (const listItem of cocktailListItem) {
@@ -20,15 +16,16 @@ function cocktailListener() {
   }
 }
 
-// Paint/render HTML
+// Pintar/renderizar el HTML
 function renderCocktailList() {
   let html = '';
-  // Const from the placeholder image
+  // Constante para la imagen por defecto
   const emptyImg =
     'https://via.placeholder.com/210x295/ffffff/666666/?text=drink';
   for (const drinkItem of drinks) {
     if (drinkItem.strDrinkThumb !== null) {
       html += `<li class="cocktail js-cocktail" id="${drinkItem.idDrink}">`;
+      // strImageSource tiene DEMASIADAS rutas null
       html += `<img alt="Cóctel" class="cocktailImg" src="${drinkItem.strDrinkThumb}" />`;
       html += `<h3>${drinkItem.strDrink}</h3>`;
       html += `</li>`;
@@ -40,18 +37,43 @@ function renderCocktailList() {
     }
     cocktailList.innerHTML = html;
 
-    // Listener for each cocktail
+    // Listener para cada cóctel
     cocktailListener();
   }
 }
 
-// Fetch cocktails from server
+// Función manejadora
+function handleClickCocktail(event) {
+  console.log(event.currentTarget.id);
+  const selectedCocktailId = event.currentTarget.id; // Obtener a qué cóctel doy clic
+
+  // ¿Existe el cóctel en el listado de favoritos?
+  const foundCocktail = drinks.find((favCocktail) => {
+    return favCocktail.idDrink === selectedCocktailId;
+  });
+
+  const foundFavoriteIndex = favoriteCocktails.findIndex((favCocktail) => {
+    return favCocktail.idDrink === selectedCocktailId;
+  });
+
+  if (foundFavoriteIndex === -1) {
+    // No encontrado
+    favoriteCocktails.push(foundCocktail);
+  } else {
+    // Eliminar de la lista de favoritos
+    favoriteCocktails.splice(foundFavoriteIndex, 1);
+  }
+  console.log(favoriteCocktails); // Ver en la consola que los cócteles se quitan y se ponen
+  renderCocktailList();
+}
+
+// Fetch al servidor de los cócteles
 fetch(
   `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch.value}`
 )
   .then((response) => response.json())
   .then((data) => {
-    // Save cocktails' info
+    // Guardar la información de los cócteles
     drinks = data.drinks;
 
     // Paint/render HTML
