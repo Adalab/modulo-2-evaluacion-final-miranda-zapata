fb5706c1ff99f7133fbff2133f51765d30fd1815
miranda-zapata <miranda.zapata@gmail.com>
2022-04-09 18:21:10 +0200

    create new branch to save first code. clean main code

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,45 @@
 'use strict';
-// Datos HTML
+// Constantes
 const cocktailList = document.querySelector('.js-cocktailList');
+const favoritesList = document.querySelector('.js-favoritesList');
 const inputSearch = document.querySelector('.js-search');
 const btnSubmit = document.querySelector('.js-submitBtn');
 const btnReset = document.querySelector('.js-resetBtn');
-
-// Constante para la imagen por defecto
 const defaultImg =
   'https://via.placeholder.com/210x295/ffffff/666666/?text=drink';
 
-// Array/lista de todas las bebidas
+// Variables
 let drinks = [];
-// Array/lista de cócteles favoritos
 let favoriteCocktails = [];
 
-// Listener (evento clic) para cada cóctel
-function cocktailListener() {
-  const cocktailListItem = document.querySelectorAll('.js-cocktail');
-  for (const listItem of cocktailListItem) {
-    listItem.addEventListener('click', handleClickCocktail);
+// Funciones
+function validateInputValue() {
+  if (inputSearch.value === '') {
+    alert('Busca un cóctel para empezar');
+    handleClickReset();
   }
 }
 
-// Pintar/renderizar la lista entera de cócteles
+function handleInputSearch(event) {
+  event.preventDefault();
+  validateInputValue();
+
+  const filterValue = inputSearch.value;
+
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filterValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      drinks = data.drinks;
+      renderCocktailList();
+    });
+}
+
 function renderCocktailList() {
   let html = '';
 
   for (const drinkItem of drinks) {
-    // Variables que dan clases para resaltar el seleccionado como favorito
     let favoriteClass = '';
     let favoriteClassName = '';
 
@@ -57,13 +69,9 @@ function renderCocktailList() {
     }
     cocktailList.innerHTML = html;
   }
-  // Listener para cada cóctel
   cocktailListener();
 }
 
-const favoritesList = document.querySelector('.js-favoritesList');
-
-// Pintar/renderizar la lista de cócteles favoritos
 function renderFavoriteCocktails() {
   let html = '';
 
@@ -83,12 +91,8 @@ function renderFavoriteCocktails() {
   favoritesList.innerHTML = html;
 }
 
-// Función manejadora
 function handleClickCocktail(event) {
-  console.log(event.currentTarget.id);
-  const selectedCocktailId = event.currentTarget.id; // Obtener a qué cóctel doy clic
-
-  // ¿Existe el cóctel en el listado de favoritos?
+  const selectedCocktailId = event.currentTarget.id;
   const foundCocktail = drinks.find((favCocktail) => {
     return favCocktail.idDrink === selectedCocktailId;
   });
@@ -98,81 +102,45 @@ function handleClickCocktail(event) {
   });
 
   if (foundFavoriteIndex === -1) {
-    // No encontrado
     favoriteCocktails.push(foundCocktail);
   } else {
-    // Eliminar de la lista de favoritos
     favoriteCocktails.splice(foundFavoriteIndex, 1);
   }
-  console.log(favoriteCocktails); // Ver en la consola que los cócteles se quitan y se ponen
+
   localStorage.setItem('favoriteCocktails', JSON.stringify(favoriteCocktails));
+
   renderCocktailList();
   renderFavoriteCocktails();
 }
 
-// Filtrar los resultados del input de búsqueda con una función manejadora del evento click del botón "buscar"
-function handleInputSearch(event) {
-  event.preventDefault();
-  validateInputValue();
-
-  // Constante para el cóctel buscado
-  const filterValue = inputSearch.value;
-
-  // Fetch al servidor de los cócteles
-  fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filterValue}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Guardar la información de los cócteles en su variable global
-      drinks = data.drinks;
-
-      // const filteredList =
-      /* drinks.filter((drinkItem) => {
-        return drinkItem.strDrink
-          .toLowerCase()
-          .includes(filterValue.toLowerCase());
-      }); */
-      // drinks = filteredList;
-      renderCocktailList();
-
-      console.log(filterValue);
-    });
+function cocktailListener() {
+  const cocktailListItem = document.querySelectorAll('.js-cocktail');
+  for (const listItem of cocktailListItem) {
+    listItem.addEventListener('click', handleClickCocktail);
+  }
 }
 
-// Local storage
-function getFavCocktailsLS() {
+function getFavLocalStorage() {
   const savedFavorites = localStorage.getItem('favoriteCocktails');
   if (savedFavorites !== null) {
     favoriteCocktails = JSON.parse(savedFavorites);
     renderFavoriteCocktails();
   }
 }
-getFavCocktailsLS();
 
-// Función manejadora para el botón de reset
 function handleClickReset(event) {
   event.preventDefault();
-
   inputSearch.value = '';
-
   // drinks = [];
   favoriteCocktails = [];
-
   cocktailList.innerHTML = '';
   favoritesList.innerHTML = '';
-
   localStorage.removeItem('favoriteCocktails');
 }
 
-// Función para verificar que el input tiene una opción por defecto (cuando no se introduce un valor)
-function validateInputValue() {
-  if (inputSearch.value === '') {
-    alert('Busca un cóctel para empezar');
-    handleClickReset();
-  }
-}
+// Llamadas a la función
+getFavLocalStorage();
 
-// Listener (evento clic) del botón de búsqueda
+// Listeners
 btnSubmit.addEventListener('click', handleInputSearch);
 btnReset.addEventListener('click', handleClickReset);
